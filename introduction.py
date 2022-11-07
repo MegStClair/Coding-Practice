ac97d9d81b5068221cbad586d860cc032ba4d4cd
Meagan St Clair <stclair.meagan@gmail.com>
2022-11-07 16:14:33 -0600

    Completed Day 4. Passed test cases.

--- introduction.py
+++ introduction.py
@@ -96,4 +96,38 @@ if __name__ == '__main__':
     # If n is even and greater than 20, print Not Weird
     if (N % 2 == 0) and N > 20:
         print("Not Weird")
-        
+
+''' DAY 4
+Task:
+Write a Person class with an instance variable, age , and a constructor that takes an integer, initialAge , as a parameter. 
+The constructor must assign initalAge to age after confirming the argument passed as initialAge is not negative; 
+if a negative argument is passed as initalAge, the constructor should set age to 0 and print Age is not valid, setting age to 0.. 
+In addition, you must write the following instance methods:
+
+1. yearPasses() should increase the age instance variable by 1.
+2. amIOld() should perform the following conditional actions:
+    If age < 13, print You are young..
+    If  age >= 13 and age < 18, print You are a teenager..
+    Otherwise, print You are old..
+'''
+
+class Person:
+    def __init__(self,initialAge):
+        if initialAge < 0:
+            self.initialAge = 0
+            print("Age is not valid, setting age to 0.")
+        else: 
+            self.initialAge = initialAge
+            
+    def amIOld(self):
+        if self.initialAge < 13:
+            print("You are young.")
+            
+        elif self.initialAge < 18:
+            print("You are a teenager.")
+            
+        else:
+            print("You are old.")
+            
+    def yearPasses(self):
+        self.initialAge += 1
