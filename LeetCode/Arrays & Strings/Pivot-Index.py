acc2ccd123dbab0cb5e197ec452cf2e00c8daf25
Meagan St Clair <stclair.meagan@gmail.com>
2022-11-16 14:16:36 -0600

    reorganized files

--- LeetCode/Arrays & Strings/Pivot-Index.py
+++ LeetCode/Arrays & Strings/Pivot-Index.py
@@ -1,21 +1,3 @@
-''' SQUARES OF SORTED ARRAY 
-
-Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.'''
-
-def sortedSquares(self, nums):
-    new = []
-    for n in nums:
-        new.append(n*n)
-    return sorted(new)
-
-    # O(n) 
-
-## OPTIMIZED ##
-def sortedSquares(self, nums):
-    return sorted(n*n for n in nums)
-
-    # O(n log n)
-
 
 ''' FIND PIVOT INDEX 
 
